463b9556fe11648cf8ce3b2d63bf1fcfd4e2487e
LitFireDrogon <a58647691@gmail.com>
2016-05-05 23:54:31 +0800

    Added files via upload

--- day5_the_puzzle/HomePage.js
+++ day5_the_puzzle/HomePage.js
@@ -1,39 +1,38 @@
-﻿$(document).ready(function () {
+﻿var puzzlePositionX;//puzzle的left值
+var puzzlePositionY;//puzzle的top值
+var isFinish = true;//true:拼塊位置正確;
+$(document).ready(function () {
+    puzzlePositionX = $(".puzzle").offset().left;//puzzle的left值
+    puzzlePositionY = $(".puzzle").offset().top;//puzzle的top值
     MakePicCell();
-    $("#ok").click(function () {
+    $("#buttonOk").click(function () {
         check();
+        if (isFinish) {
+            changePic();
+        }
+        else {
+            alert("Check again!!");
+        }
+    });
+    $("#again").click(function () {
+        window.location.reload();
     });
 });
 function MakePicCell() {
-    //填入4張圖
-    //for (var i = 0; i < 4; i++) {
-    //    $(".puzzle").append("<div class='piccell' id='pic" + i + "'><img src='../resource/litfiredragon.jpg'/></div>");
-    //    var row = parseInt(i / 2);
-    //    var col = i % 2;
-    //    var picNum = "#pic"+i;
-    //    $("#pic" + i + " img").css("margin-left", col * -110 + 1).css("margin-top", row * -110 + 1);
-    //    $("#pic" + i).draggable({
-    //        containment: '.gamearea',//可以拖拉的範圍
-    //        stack: picNum,//使拖拉的目標顯示在最上層,z-index=0
-    //        snap: '.puzzle',//貼齊的對象
-    //        snapMode: 'inner',//貼齊的模式,inner,outer,both
-    //        snapTolerance: 20,//貼齊的觸發距離
-    //    });
-
-    //}
     for (var i = 0; i < 16; i++) {
         $(".puzzle").append("<div class='piccell' id='pic" + i + "'><img src='../resource/litfiredragon44.jpg'/></div>");
         var row = parseInt(i / 4);
         var col = i % 4;
         var picNum = "#pic" + i;
-        var index = Math.floor(Math.random() * 16 + 1);
-        $("#pic" + i + " img").css("margin-left", col * -110 + 1).css("margin-top", row * -110 + 1);
-        if (index % 2 > 0)
-        {
-            $(picNum).css({ "top":index * 3, "left": 448 });
+
+
+        $("#pic" + i + " img").css("margin-left", col * -110 + 1).css("margin-top", row * -110 + 1).css("z-index", i);
+
+        if (i % 2 > 0) {
+            $(picNum).css({ "top": puzzlePositionY , "left": puzzlePositionX + i * 30 });
         }
         else {
-            $(picNum).css({ "top":index * 3, "left": 448 });
+            $(picNum).css({ "top": puzzlePositionY + 100, "left": puzzlePositionX + i *30 });
         }
         $("#pic" + i).draggable({
             containment: 'window',//可以拖拉的範圍
@@ -44,16 +43,38 @@ function MakePicCell() {
         });
     }
 }
+//109.5,80.875
 function check() {
     for (var i = 0; i < 16; i++) {
-        
-        
-        //$(picNum).val(myOffset.left);
+        var row = parseInt(i / 4);
+        var col = i % 4;
         var picNum = "#pic" + i;
-       
-        
+
+        var picPositionX = $(picNum).offset().left;
+        var picPositionY = $(picNum).offset().top;
+        //判斷各片拼圖的位置對不對,允許差距+-10pix
+        if (Math.abs(Math.abs(puzzlePositionX - picPositionX) - col * 110) > 10) {
+            isFinish = false;
+        }
+        else if (Math.abs(Math.abs(puzzlePositionY - picPositionY) - row * 110) > 10) {
+            isFinish = false;
+        }
+        else if (row == 0 && col == 0) {
+            if (Math.abs(puzzlePositionX - picPositionX) > 10) {
+                isFinish = false;
+            }
+            if (Math.abs(puzzlePositionY - picPositionY) > 10) {
+                isFinish = false;
+            }
+        }
     }
-    var positionX = $("#pic1").offset().left;
-    var positionY = $("#pic1").offset().top;
-    alert(positionX +" ,"+ positionY);
+
+
+}
+function changePic() {
+    $(".piccell").fadeOut(2000);
+    $("#anser").css("z-index", 0);
+    $(".gamearea").css("margin", "auto");
+    $("#buttonOk").fadeOut();
+    document.getElementById("message").innerHTML = "Congratulation!!";
 }
